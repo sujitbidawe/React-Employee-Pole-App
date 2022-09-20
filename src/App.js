cc768a40474b24fb52b7cd24b48ca02d8ea9eb0d
sujitbidawe <bidawesujit@gmail.com>
2022-09-20 19:54:20 +0530

    ADDED: code to fetch initial data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import { handleInitialData } from './actions/shared';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = (props) => {
+	  useEffect(() => {
+		  props.dispatch(handleInitialData());
+	}, [])
 
-export default App;
+	return <div>Starter Code</div>;
+};
+
+export default connect()(App);
