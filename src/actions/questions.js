3b477d3bd59d635b1122471009b386852aaeae2e
sujitbidawe <bidawesujit@gmail.com>
2022-09-23 03:05:16 +0530

    ADDED: code to add new poll

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,34 @@
+import { showLoading, hideLoading } from "react-redux-loading-bar";
+import { _saveQuestion } from "../utils/_DATA";
+
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
+export const ADD_QUESTION = 'ADD_QUESTION';
 
 export function receiveQuestions(questions) {
     return {
         type: RECEIVE_QUESTIONS,
         questions
+    }
+}
+
+export function addQuestion(question) {
+    return {
+        type: ADD_QUESTION,
+        question
+    }
+}
+
+export function handleAddQuestion(questionObj) {
+    return (dispatch, getState) => {
+        const { authedUser } = getState();
+
+        dispatch(showLoading());
+
+        return _saveQuestion({
+            ...questionObj,
+            author: authedUser
+        })
+            .then((question) => dispatch(addQuestion(question)))
+            .then(() => dispatch(hideLoading()))
     }
 }
--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,7 +1,30 @@
-const NewQuestion = () => {
+import { useState } from "react";
+import { connect } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { handleAddQuestion } from "../actions/questions";
+
+const NewQuestion = (props) => {
+
+    const navigate = useNavigate();
+
+    const [optionOneText, setoptionOneText] = useState('');
+    const [optionTwoText, setoptionTwoText] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        props.dispatch(handleAddQuestion({ optionOneText, optionTwoText }));
+        navigate('/');
+    }
+
     return (
-        <div>NewQuestion</div>
+        <div className="container">
+            <h4>Would You Rather...?</h4>
+            <input type='text' value={optionOneText} placeholder='Enter first option' onChange={(e) => { setoptionOneText(e.target.value.trim()) }} />
+            <input type='text' value={optionTwoText} placeholder='Enter second option' onChange={(e) => { setoptionTwoText(e.target.value.trim()) }} />
+            <button className="primary-button" disabled={optionOneText === '' || optionTwoText === ''} onClick={(e) => { handleSubmit(e) }}>Add Question</button>
+        </div>
     )
 }
 
-export default NewQuestion;
+export default connect()(NewQuestion);
